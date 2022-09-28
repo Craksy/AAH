086fd34a5e524caf2895936a0bf45b4dee2dee4f
Amnesia <silaswagner.dev@gmail.com>
2022-09-28 13:28:27 +0200

    add frontend

---
+++ AvaloniaPlayground/ViewModels/ViewModelBase.cs
@@ -0,0 +1,5 @@
+﻿using ReactiveUI;
+
+namespace AvaloniaPlayground.ViewModels {
+    public class ViewModelBase : ReactiveObject { }
+}
---
+++ AvaloniaPlayground/Views/AuctionPage.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+using AvaloniaPlayground.ViewModels;
+using ReactiveUI;
+
+namespace AvaloniaPlayground.Views;
+
+public partial class AuctionPage : ReactiveUserControl<AuctionPageViewModel> {
+    public AuctionPage() {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent() {
+        this.WhenActivated(disposables => { AvaloniaXamlLoader.Load(this); });
+        AvaloniaXamlLoader.Load(this);
+    }
+}
