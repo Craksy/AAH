348af43f20ec71c25e4ce0bd4e1ccba76e9abc7a
Amnesia <silaswagner.dev@gmail.com>
2022-09-30 09:21:10 +0200

    add frontend

---
+++ Frontend/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,11 @@
+﻿using ReactiveUI;
+
+namespace Frontend.ViewModels; 
+
+public class DashboardViewModel : ReactiveObject, IRoutableViewModel{
+    public IScreen HostScreen { get; }
+    public string? UrlPathSegment { get; } = "Dashboard";
+
+    public DashboardViewModel() {
+    }
+}
---
+++ Frontend/ViewModels/HistoryViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+using Avalonia.Collections;
+using MaterialDesign.Avalonia.PackIcon;
+using ReactiveUI;
+
+namespace Frontend.ViewModels;
+
+public class HistoryViewModel : ReactiveObject, IRoutableViewModel {
+    public IScreen HostScreen { get; } = null!;
+    public string? UrlPathSegment { get; } = "History";
+
+    public AvaloniaList<TabItemViewModel> TabItems { get; set; } = new();
+
+    public HistoryViewModel() {
+        TabItems.Add(new TabItemViewModel("Dashboard", new DashboardViewModel(), PackIconKind.ViewDashboard));
+        TabItems.Add(new TabItemViewModel("Profile", new ProfileViewModel(), PackIconKind.Settings));
+        
+    }
+
+
+}
