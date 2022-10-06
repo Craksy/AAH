a12d5f7b90de78d8f408db2f5818f04cc32334cf
aliennotfound <adamdreioe@gmail.com>
2022-10-06 12:35:49 +0200

    Work on auction datagrid

--- Frontend/ViewModels/DashboardViewModel.cs
+++ Frontend/ViewModels/DashboardViewModel.cs
@@ -1,69 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
 using System.Net.Mime;
+using AutoAuctionProjekt;
+using AutoAuctionProjekt.Classes;
 using AutoAuctionProjekt.Classes.Data;
 using AutoAuctionProjekt.Classes.Vehicles;
+using Avalonia.Collections;
+using Avalonia.Controls;
+using Avalonia.Controls.Templates;
 using Frontend.Views;
 using ReactiveUI;
 using SkiaSharp;
 
 namespace Frontend.ViewModels; 
 
-public class DashboardViewModel : ReactiveObject {
+    public record TestAuction(Vehicle vehicle, ISeller seller, decimal minimumPrice);
+public class DashboardViewModel : ReactiveObject, IDataTemplate {
     public IScreen HostScreen { get; }
     public string? UrlPathSegment { get; } = "Dashboard";
-    public ObservableCollection<CurrentAuction> CurrentAuctions { get; }
     static Database _db = Database.Instance;
-    
+    // private string _test { get; set; } = _db.GetCurrentAuctions()[1].Seller.UserName;
+
+    public AvaloniaList<Auction> CurrentAuctions { get; set; }
+
+    public IControl Build(object Username)
+    {
+        return new TextBlock() { Text = (string)Username };
+    }
+
+    public bool Match(object data)
+    {
+        return data is string;
+    }
+
     public DashboardViewModel()
     {
-        CurrentAuctions = new ObservableCollection<CurrentAuction>(GenerateMockCurrentAuctionTable());
-    }
-
-    public class CurrentAuction
-    {
-        public int DepartmentNumber { get; set; }
+        CurrentAuctions = new AvaloniaList<Auction>();
+        CurrentAuctions.AddRange(_db.GetCurrentAuctions());
         
-        public string DeskLocation{ get; set; }
-        
-        public int EmployeeNumber { get; set; }
-
-        public string FirstName { get; set; }
-        
-        public string LastName { get; set; }
-    }
-    
-    private IEnumerable<CurrentAuction> GenerateMockCurrentAuctionTable()
-    {
-        var currentAuction = new List<CurrentAuction>()
-        {
-            new CurrentAuction()
-            {
-                FirstName = "Pat", 
-                LastName = "Patterson", 
-                EmployeeNumber = 1010,
-                DepartmentNumber = 100, 
-                DeskLocation = "B3F3R5T7"
-            },
-            new CurrentAuction()
-            {
-                FirstName = "Jean", 
-                LastName = "Jones", 
-                EmployeeNumber = 973,
-                DepartmentNumber = 200, 
-                DeskLocation = "B1F1R2T3"
-            },
-            new CurrentAuction()
-            {
-                FirstName = "Terry", 
-                LastName = "Tompson", 
-                EmployeeNumber = 300,
-                DepartmentNumber = 100, 
-                DeskLocation = "B3F2R10T1"
-            }
-        };
-
-        return currentAuction;
+        // CurrentAuctions = new ObservableCollection<CurrentAuction>(GenerateMockCurrentAuctionTable());
+        // _db.GetCurrentAuctions();
     }
 }
